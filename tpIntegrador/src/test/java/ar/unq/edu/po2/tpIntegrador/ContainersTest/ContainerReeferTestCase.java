cf01007e0712e479b3d5b2f7ea8bc2e9fd03d4f2
marcos-ordo <ordonezmarcos935@gmail.com>
2025-10-29 10:14:20 -0300

    terminado test de container y subido .gitignore para evitar merges inecesarios en el futuro

--- tpIntegrador/src/test/java/ar/unq/edu/po2/tpIntegrador/ContainersTest/ContainerReeferTestCase.java
+++ tpIntegrador/src/test/java/ar/unq/edu/po2/tpIntegrador/ContainersTest/ContainerReeferTestCase.java
@@ -15,18 +15,7 @@ class ContainerReeferTestCase {
 	@BeforeEach
 	void setUp() throws Exception {
 		blBasico = mock(BLBasico.class);
-		this.containerReefer = new ContainerReefer("ABCD0000001", 20, 40, 10, 500, false, blBasico);
-	}
-
-	@Test
-	void testGetPeso(){
-		when(blBasico.getPeso()).thenReturn(500);
-		assertEquals(blBasico.getPeso(), this.containerReefer.getPeso());
-	}
-	
-	@Test
-	void testGetContenido() {
-		assertEquals(blBasico, this.containerReefer.getContenido());
+		this.containerReefer = new ContainerReefer("ABCD0000001", 20, 40, 10, 500, false, this.blBasico);
 	}
 	
 	@Test
@@ -37,5 +26,36 @@ class ContainerReeferTestCase {
 	@Test
 	void testGetAncho() {
 		assertEquals(20, this.containerReefer.getAncho());
+	}
+	
+	@Test
+	void testGetLargo() {
+		assertEquals(40, this.containerReefer.getLargo());
+	}
+	
+	@Test
+	void testGetMetroCubico() {
+		assertEquals(8000, this.containerReefer.getMetroCubico());
+	}
+	
+	@Test
+	void testGetAltura() {
+		assertEquals(10, this.containerReefer.getAltura());
+	}
+	
+	@Test
+	void testGetPeso(){
+		when(this.blBasico.getPeso()).thenReturn(500);
+		assertEquals(this.blBasico.getPeso(), this.containerReefer.getPeso());
+	}
+	
+	@Test
+	void testGetEsDesconsolidado(){
+		assertEquals(false, this.containerReefer.getEsDesconsolidado());
+	}
+	
+	@Test
+	void testGetContenido() {
+		assertEquals(this.blBasico, this.containerReefer.getContenido());
 	}
 }

5522d8f677148dce0ca4149a8a6b853aa0f69e3c
Lagomarsino, Iara Belén <lagomarsinoiara@gmail.com>
2025-10-29 00:06:00 -0300

    Hecho Test de la clase Tramo

---
+++ tpIntegrador/src/test/java/ar/unq/edu/po2/tpIntegrador/NavieraTest/TramoTestCase.java
@@ -0,0 +1,47 @@
+package ar.unq.edu.po2.tpIntegrador.NavieraTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import ar.unq.edu.po2.tpIntegrador.Naviera.Tramo;
+import ar.unq.edu.po2.tpIntegrador.Terminal.Terminal;
+
+class TramoTestCase {
+	private Tramo tramo;
+	private Terminal terminalOrig;
+	private Terminal terminalDes;
+	
+
+	@BeforeEach
+	void setUp() throws Exception {
+		terminalOrig = mock(Terminal.class);
+		terminalDes = mock(Terminal.class);
+		
+		tramo = new Tramo(2, 500d, terminalOrig, terminalDes);
+	}
+
+	@Test
+	void testGetSemana() {
+		assertEquals(2, tramo.getSemanas());
+	}
+	
+	@Test
+	void testGetPrecio() {
+		assertEquals(500d, tramo.getPrecio());
+	}
+	
+	@Test
+	void testGetOrigen() {
+		assertEquals(terminalOrig, tramo.getOrigen());
+	}
+
+	@Test
+	void testGetDestino() {
+		assertEquals(terminalDes, tramo.getDestino());
+	}
+
+
+
+}

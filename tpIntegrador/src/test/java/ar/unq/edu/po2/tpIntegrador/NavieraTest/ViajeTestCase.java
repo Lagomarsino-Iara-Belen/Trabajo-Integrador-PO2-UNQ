748f069c22f32ea635671f41f788a0bd5f55ffc5
marcos-ordo <ordonezmarcos935@gmail.com>
2025-10-31 17:42:16 -0300

    arreglo del test getProximaParada() de viaje

--- tpIntegrador/src/test/java/ar/unq/edu/po2/tpIntegrador/NavieraTest/ViajeTestCase.java
+++ tpIntegrador/src/test/java/ar/unq/edu/po2/tpIntegrador/NavieraTest/ViajeTestCase.java
@@ -54,8 +54,10 @@ class ViajeTestCase {
 		when(tramo.getPuertoOrigen()).thenReturn(terminal1);
 		when(tramo.getPuertoDestino()).thenReturn(terminal2);
 		
-		when(circuito.getTodosLosTramos())
-				.thenReturn(new ArrayList<>(List.of(tramo)));
+		when(circuito.proximaParadaDe(terminal1))
+				.thenReturn(terminal2);
+		when(circuito.haySiguienteParada(terminal1))
+				.thenReturn(true);
 		
 		viaje.proximaParada(terminal1);
 		
